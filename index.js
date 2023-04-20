662de06f1bca42cc12aeaefcfb452657b6991cf6
pbaki <pbaki999@gmail.com>
2023-04-20 21:10:59 +0200

    Added random cat gif generator and on phrase gif generator

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=limsk8DThW5otN0v31Ciiokn2kgW4gfe&s=cats",
-  {
-    mode: "cors",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+const changeButton = document.getElementById("imgbutton");
+
+fetchImage();
+
+function fetchImage() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&s=cats",
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+changeButton.addEventListener("click", fetchImage);
+
+//Input button
+
+const searchButton = document.getElementById("searchbutton");
+searchButton.addEventListener("click", searchButtonFunctionality);
+
+function searchButtonFunctionality(e) {
+  const inputText = document.getElementById("search").value;
+
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&q=${inputText}`,
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      let randomGif = Math.floor(Math.random() * response.data.length);
+      img.src = response.data[randomGif].images.original.url;
+      console.log(img.src);
+    })
+    .catch(function (err) {
+      console.log("No gifs with that query");
+      img.src = "";
+      img.alt = "No gifs with that query :(";
+    });
+  e.preventDefault();
+}
