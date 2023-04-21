ddf22a05347b3d3127c9c7e425457ac9690e8e4c
pbaki <pbaki999@gmail.com>
2023-04-21 17:41:03 +0200

    now using async await

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const changeButton = document.getElementById("imgbutton");
 
 fetchImage();
 
-function fetchImage() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&s=cats",
-    {
-      mode: "cors",
-    }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
+async function fetchImage() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&s=cats",
+      {
+        mode: "cors",
+      }
+    );
+    const responseData = await response.json();
+    img.src = responseData.data.images.original.url;
+  } catch (error) {
+    img.alt = "Error loading an Image";
+    console.log(error);
+  }
 }
 
 changeButton.addEventListener("click", fetchImage);
@@ -26,27 +27,23 @@ const searchButton = document.getElementById("searchbutton");
 searchButton.addEventListener("click", searchButtonFunctionality);
 
 function searchButtonFunctionality(e) {
-  const inputText = document.getElementById("search").value;
-
-  fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&q=${inputText}`,
-    {
-      mode: "cors",
-    }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-      let randomGif = Math.floor(Math.random() * response.data.length);
-      img.src = response.data[randomGif].images.original.url;
-      console.log(img.src);
-    })
-    .catch(function (err) {
-      console.log("No gifs with that query");
-      img.src = "";
-      img.alt = "No gifs with that query :(";
-    });
   e.preventDefault();
+  const inputText = document.getElementById("search").value;
+  async function fetchData() {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&q=${inputText}`,
+      {
+        mode: "cors",
+      }
+    );
+
+    const responseData = await response.json();
+    let randomGif = Math.floor(Math.random() * responseData.data.length);
+    img.src = responseData.data[randomGif].images.original.url;
+  }
+  fetchData().catch(function (err) {
+    console.log("No gifs with that query");
+    img.src = "";
+    img.alt = "No gifs with that query :(";
+  });
 }
